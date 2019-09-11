01c2b8783ff74db38230cf7d59bab5dcea03d183
Joost van der Griendt <joostvdg@gmail.com>
2019-09-11 23:31:51 +0200

    update welcome message
    Signed-off-by: Joost van der Griendt <joostvdg@gmail.com>

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X golang http example"
+	title := "Welcome Kubernets Day!"
 
 	from := ""
 	if r.URL != nil {
